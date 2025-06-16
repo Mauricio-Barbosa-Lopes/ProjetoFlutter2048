5788c02b30f06ad39905e61efae394afd99b668a
Mauricio-Barbosa-Lopes <mauriciolopes.cm016@academico.ifsul.edu.br>
2025-06-16 11:55:46 -0300

    Add files via upload
    16/06/2025

--- lib/main.dart
+++ lib/main.dart
@@ -1,132 +1,285 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ClassePai());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ClassePai extends StatefulWidget{
+  /*ATRIBUTOS*/
+  int vitoria_aconteceu = 0; //0 nada aconteceu, 1 vitoria 2 derrota
+  List<List<int>> tabuleiro_facil = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]];
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    String modojogo = "facil";
-    List<List<int>> tabuleiroJogo;
-    if(modojogo == "facil"){
-      tabuleiroJogo = [[0,0,0,0],
-                       [0,0,1,0],
-                       [0,0,0,0],
-                       [0,0,0,0]];
+  void botaoParaBaixo(){
+    /*o que fazer quando o botao pra baixo for clicado*/
 
+    for(int i = 3; i >= 0; i--){
+      if( i == 3){
+        //calculo a soma, se possível
+
+      }
+      else {
+        //desloco as pecas
+        tabuleiro_facil[i][3] = tabuleiro_facil[i][2];
+        tabuleiro_facil[i][2] = tabuleiro_facil[i][1];
+        tabuleiro_facil[i][1] = tabuleiro_facil[i][0];
+        tabuleiro_facil[i][0] = 0;
+      }
+    }
+
+
+    colocaNovaPeca();
+  }
+
+  void colocaNovaPeca(){
+    bool nova_peca_adicionada = false;
+    do {
+      int x = Random().nextInt(4);
+      int y = Random().nextInt(4);
+      if (tabuleiro_facil[x][y] == 0) {
+        tabuleiro_facil[x][y] = 1;
+        nova_peca_adicionada = true;
+      }
+    }while(nova_peca_adicionada == false);
+  }
+
+  /*Chama a classe filha*/
+  State<ClassePai> createState(){
+
+    return ClasseFilha();
+  }
+}
+
+class ClasseFilha extends State<ClassePai>{
+  Widget build(BuildContext context){
+
+    String textoVitoria = "";
+    List<List<int>> tabuleiro = widget.tabuleiro_facil;
+    if(widget.vitoria_aconteceu == 1){
+      textoVitoria = "VOCE VENCEU";
+    }
+    else if(widget.vitoria_aconteceu == 2){
+      textoVitoria = "VOCE PERDEU";
     }
     else{
-      tabuleiroJogo = [[0,0,0,0],
-        [0,0,1,0],
-        [0,0,0,0],
-        [0,0,0,0]];
+      textoVitoria = "";
     }
 
-    //da esquerda pra direira
-    tabuleiroJogo[1][3] = tabuleiroJogo[1][2];
-    tabuleiroJogo[1][2] = tabuleiroJogo[1][1];
-    tabuleiroJogo[1][1] = tabuleiroJogo[1][0];
-    tabuleiroJogo[1][0] = 0;
-
-
-    //colunas = Column(children:,)
-    //linhas = Row(children:,)
-    //texto = Text()
-    //botoes = TextButton(onPressed:(){},child:,)
-    //containers = Container(child:,)
-
-
-
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
+      home: Column(
+        children: [
+          Row(
+            children: [
+              Text(
+                "Jogo 2048", // Corrigido o erro de digitação também
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.purple, // Altere para a cor desejada
+                ),
+              ), Text("   ", style: TextStyle(fontSize: 20),),
+              Text(
+                "Movimentos:", // Corrigido o erro de digitação também
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.blue, // Altere para a cor desejada
+                ),
+              ),
+            ],
+          ),
+          Text(textoVitoria, style: TextStyle(fontSize: 20),),
+          Row(
+            children: [
+              ElevatedButton(
+                  onPressed: (){
+                           },
+                  child: Text("Nivel 1")
+              ),
+              ElevatedButton(onPressed: (){}, child: Text("Nivel 2")),
+              ElevatedButton(onPressed: (){}, child: Text("Nivel 3")),
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                color: Colors.white,
+                child: Text("${tabuleiro[0][0]}"),
+                width: 50,
+                height: 50,
+              ),
+              Container(
+                color: Colors.white,
+                child: Text("${tabuleiro[1][0]}"),
+                width: 50,
+                height: 50,
+              ),
+              Container(
+                color: Colors.white,
+                child: Text("${tabuleiro[2][0]}"),
+                width: 50,
+                height: 50,
+              ),
+              Container(
+                color: Colors.white,
+                child: Text("${tabuleiro[3][0]}"),
+                width: 50,
+                height: 50,
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                color: Colors.white,
+                child: Text("${tabuleiro[0][1]}"),
+                width: 50,
+                height: 50,
+              ),
+              Container(
+                color: Colors.white,
+                child: Text("${tabuleiro[1][1]}"),
+                width: 50,
+                height: 50,
+              ),
+              Container(
+                color: Colors.white,
+                child: Text("${tabuleiro[2][1]}"),
+                width: 50,
+                height: 50,
+              ),
+              Container(
+                color: Colors.white,
+                child: Text("${tabuleiro[3][1]}"),
+                width: 50,
+                height: 50,
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                color: Colors.white,
+                child: Text("${tabuleiro[0][2]}"),
+                width: 50,
+                height: 50,
+              ),
+              Container(
+                color: Colors.white,
+                child: Text("${tabuleiro[1][2]}"),
+                width: 50,
+                height: 50,
+              ),
+              Container(
+                color: Colors.white,
+                child: Text("${tabuleiro[2][2]}"),
+                width: 50,
+                height: 50,
+              ),
+              Container(
+                color: Colors.white,
+                child: Text("${tabuleiro[3][2]}"),
+                width: 50,
+                height: 50,
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                color: Colors.white,
+                child: Text("${tabuleiro[0][3]}"),
+                width: 50,
+                height: 50,
+              ),
+              Container(
+                color: Colors.white,
+                child: Text("${tabuleiro[1][3]}"),
+                width: 50,
+                height: 50,
+              ),
+              Container(
+                color: Colors.white,
+                child: Text("${tabuleiro[2][3]}"),
+                width: 50,
+                height: 50,
+              ),
+              Container(
+                color: Colors.white,
+                child: Text("${tabuleiro[3][3]}"),
+                width: 50,
+                height: 50,
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Container(
 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+            Text("    "),
+              Container(
+                  child:
+
+                  IconButton(
+                    icon: Icon(Icons.arrow_upward), // Ícone de seta para frente
+                    color: Colors.black,             // Cor do ícone
+                    iconSize: 30,                    // Tamanho do ícone
+                    onPressed: () {
+                      // Ação quando o botão é pressionado
+
+                    },
+                  )
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                  child:
+                  IconButton(
+                    icon: Icon(Icons.arrow_back), // Ícone de seta para frente
+                    color: Colors.black,             // Cor do ícone
+                    iconSize: 30,                    // Tamanho do ícone
+                    onPressed: () {
+                      // Ação quando o botão é pressionado
+
+                    },
+                  )
+              ),
+              Container(
+                  child:
+                  IconButton(
+                    icon: Icon(Icons.arrow_downward), // Ícone de seta para frente
+                    color: Colors.black,             // Cor do ícone
+                    iconSize: 30,                    // Tamanho do ícone
+                    onPressed: () {
+                      // Ação quando o botão é pressionado
+                      setState(() {
+                        widget.botaoParaBaixo();
+                      });
+                    },
+                  )
+              ),
+              Container(
+                  child:
+                  IconButton(
+                    icon: Icon(Icons.arrow_forward), // Ícone de seta para frente
+                    color: Colors.black,             // Cor do ícone
+                    iconSize: 30,                    // Tamanho do ícone
+                    onPressed: () {
+                      // Ação quando o botão é pressionado
+
+                    },
+                  )
+              ),
+            ],
+          ),
+        ],
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
